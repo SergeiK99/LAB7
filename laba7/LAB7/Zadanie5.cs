ab4ba33ec57f1f22599450e383d4aa1e19a34eed
SergeiK99 <skx2013@mail.ru>
2023-01-05 07:13:28 +0700

    Я напортачил с загрузкой репозитория в 7 лабе

---
+++ laba7/LAB7/Zadanie5.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace LAB7
+{
+    public class Zadanie5
+    {
+        public long z5(string numberStr) // Вроде работает, но мне кжется много чего лишнего переделал
+        {
+            bool IsDigit = numberStr.Length == numberStr.Where(c => char.IsDigit(c)).Count();
+            if (!IsDigit)
+                throw new ArgumentException();
+            long number = long.Parse(numberStr);
+            long sum = 0;
+            while (number > 0)
+            {
+
+                sum = sum + number % 10;
+                number = number / 10;
+
+            }
+            return sum;
+        }
+    }
+}
